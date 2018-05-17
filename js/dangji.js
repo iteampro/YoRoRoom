41e1a0692d64e3f1a72518c67487252fc16fa57d
Leeou <zxyxueyuan@163.com>
2018-05-18 00:23:58 +0800

    首页样式修改，加入当季热卖页面

---
+++ js/dangji.js
@@ -0,0 +1,18 @@
+/**
+ * Created by leeou on 2018/5/17.
+ */
+function a(){
+    document.getElementById("img1").src = "images/loop0.jpg";
+}
+function b(){
+    document.getElementById("img1").src = "images/loop1.jpg";
+
+}
+function c(){
+    document.getElementById("img1").src = "images/loop2.jpg";
+
+}
+
+function detail() {
+    window.location.href = "detail.html";
+}
